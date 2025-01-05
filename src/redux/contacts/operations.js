f8ed4b8b7333ae95691fa01d953c37e5c39053b8
crystaller347 <qwertymum04@gmail.com>
2025-01-05 14:12:54 +0200

    add auth

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://675684d711ce847c992d0680.mockapi.io';
+axios.defaults.baseURL = 'https://connections-api.goit.global/';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from './contacts/slice.js'
 import filtersReducer from './filters/slice.js'
+import authReducer from './auth/slice.js'
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filters: filtersReducer,
+        auth: authReducer,
     },
 });
