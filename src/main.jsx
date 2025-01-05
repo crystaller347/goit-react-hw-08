9836319f641303e462ef9b23a4a8cd5e9f03e303
crystaller347 <qwertymum04@gmail.com>
2025-01-05 15:09:55 +0200

    add Redux Persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "modern-normalize";
-import App from './components/App/App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from "./redux/store";
+import App from "./components/App/App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactsReducer from './contacts/slice.js'
-import filtersReducer from './filters/slice.js'
-import authReducer from './auth/slice.js'
+import contactsReducer from './contacts/slice.js';
+import filtersReducer from './filters/slice.js';
+import authReducer from './auth/slice.js';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+  import storage from 'redux-persist/lib/storage';
+
+  const authPersistConfig = {
+    key: 'auth',
+    storage,
+    whitelist: ['token'],
+  };
 
 export const store = configureStore({
     reducer: {
         contacts: contactsReducer,
         filters: filtersReducer,
-        auth: authReducer,
+        auth: persistReducer(authPersistConfig, authReducer),
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+        devTools: import.meta.env.MODE === 'development',
 });
+
+export const persistor = persistStore(store);
